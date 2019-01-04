938530f04a893b3eb6452cbb60803d235b4b33a7
Asim Aslam <asim@aslam.me>
2019-01-04 16:54:23 +0000

    add some handlers

---
+++ handler/udp/udp.go
@@ -0,0 +1,27 @@
+// Package udp reads and write from a udp connection
+package udp
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"path/filepath"
+)
+
+type Handler struct{}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c, err := net.Dial("udp", r.Host)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	go io.Copy(c, r.Body)
+	// write response
+	io.Copy(w, c)
+}
+
+func (h *Handler) String() string {
+	return "udp"
+}
